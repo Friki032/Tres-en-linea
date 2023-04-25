a8a39499d990c933ba83353ea878343962f04cf6
Friki032 <116515156+Friki032@users.noreply.github.com>
2023-04-25 16:56:04 -0500

    Version 1

---
+++ 3_En_Linea/src/pkg3_en_linea/Main.java
@@ -0,0 +1,17 @@
+
+package pkg3_en_linea;
+
+/**
+ *
+ * @author Saday G
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
